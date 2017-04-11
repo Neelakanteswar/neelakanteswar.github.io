93ca04a70afc9dc50503210f17ec5a0948a08b79
Neelakanteswar Patnaik <patnaikneelakanteswar@gmail.com>
2017-04-11 23:41:51 +0530

    Create splash.js

---
+++ splash.js
@@ -0,0 +1,46 @@
+<script type="text/javascript">
+function showme(){
+	document.getElementById("logo").style.opacity = "1";
+}
+setTimeout("showme()",1000);
+function stopme(){
+	document.getElementById("logo").style.animation = "none";
+}
+setTimeout("stopme()",5000);
+function showIt() {
+  document.getElementById("hello").style.visibility = "visible";
+}
+setTimeout("showIt()", 1000); // after 1 sec
+function hideIt() {
+	document.getElementById("hello").style.visibility = "hidden";
+}
+setTimeout("hideIt()",2000);
+function showIt2() {
+  document.getElementById("hello2").style.visibility = "visible";
+}
+setTimeout("showIt2()", 2000); // after 5 secs
+function hideIt2() {
+	document.getElementById("hello2").style.visibility = "hidden";
+}
+setTimeout("hideIt2()",3000);
+function showIt3() {
+  document.getElementById("hello3").style.visibility = "visible";
+}
+setTimeout("showIt3()", 3000); // after 1 sec
+function hideIt3() {
+	document.getElementById("hello3").style.visibility = "hidden";
+}
+setTimeout("hideIt3()",4000);
+function showIt4() {
+  document.getElementById("hello4").style.visibility = "visible";
+}
+setTimeout("showIt4()", 4000); // after 1 sec
+function hideIt4() {
+	document.getElementById("hello4").style.visibility = "hidden";
+}
+setTimeout("hideIt4()",5000);
+function showhome(){
+	document.getElementById("intro-text").style.opacity = "1";
+}
+setTimeout("showhome()",5000);
+</script>
