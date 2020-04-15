c338df3b4ec3598939d125ab2a86cff59b49dbc4
Neel <patnaikneelakanteswar@gmail.com>
2020-04-15 10:42:21 +1000

    Update description.js

--- assets/js/description.js
+++ assets/js/description.js
@@ -1,44 +1,78 @@
-// $(document).scroll(function() {
-//   var y = $(this).scrollLeft();
+// if($(window).width() < 1000){
+//     $(document).scroll(function() {
+//       var y = $(this).scrollLeft();
 //
-//   if (y < 1000) {
-//     $(".text1").removeClass("no_text");
-//     $(".text1").addClass("add_text");
-//     $(".text2").addClass("no_text");
-//   }
+//       if (y < 659) {
+//         $(".text1").removeClass("no_text");
+//         $(".text1").addClass("add_text");
+//         $(".text2").addClass("no_text");
+//       }
 //
-//   else if (y > 1000 && y < 3500){
-//     $(".text1").addClass("no_text");
-//     $(".text2").removeClass("no_text");
-//     $(".text2").addClass("add_text");
-//   }
+//       else if (y > 659 && y < 1712){
+//         $(".text1").addClass("no_text");
+//         $(".text2").removeClass("no_text");
+//         $(".text2").addClass("add_text");
+//       }
 //
-//   else{
-//     $(".text2").removeClass("add_text");
-//     $(".text2").addClass("no_text");
-//   }
-// });
+//       else{
+//         $(".text2").removeClass("add_text");
+//         $(".text2").addClass("no_text");
+//       }
+//     });
+// }
+//
+// else if($(window).width() > 1000){
+//     $(document).scroll(function() {
+//       var y = $(this).scrollLeft();
+//
+//       if (y < 1000) {
+//         $(".text1").removeClass("no_text");
+//         $(".text1").addClass("add_text");
+//         $(".text2").addClass("no_text");
+//       }
+//
+//       else if (y > 1000 && y < 3500){
+//         $(".text1").addClass("no_text");
+//         $(".text2").removeClass("no_text");
+//         $(".text2").addClass("add_text");
+//       }
+//
+//       else{
+//         $(".text2").removeClass("add_text");
+//         $(".text2").addClass("no_text");
+//       }
+//     });
+// }
+
+
+
+
+function text1(){
+  $(".text1").addClass("no_text");
+  $(".text2").removeClass("no_text");
+  $(".text2").addClass("add_text");
+}
+setTimeout(text1, 2000);
+
+function text2(){
+  $(".text2").removeClass("add_text");
+  $(".text2").addClass("no_text");
+  $(".text3").addClass("add_text");
+}
+setTimeout(text2, 5000);
+
+// function text3(){
+//   $(".text3").removeClass("add_text");
+// }
+// setTimeout(text3, 7000);
 
 
 if($(window).width() < 1000){
     $(document).scroll(function() {
       var y = $(this).scrollLeft();
 
-      if (y < 659) {
-        $(".text1").removeClass("no_text");
-        $(".text1").addClass("add_text");
-        $(".text2").addClass("no_text");
-      }
-
-      else if (y > 659 && y < 1712){
-        $(".text1").addClass("no_text");
-        $(".text2").removeClass("no_text");
-        $(".text2").addClass("add_text");
-      }
-
-      else{
-        $(".text2").removeClass("add_text");
-        $(".text2").addClass("no_text");
+      if (y > 659) {
+        $(".text3").removeClass("add_text");
       }
     });
 }
@@ -47,49 +81,8 @@ else if($(window).width() > 1000){
     $(document).scroll(function() {
       var y = $(this).scrollLeft();
 
-      if (y < 1000) {
-        $(".text1").removeClass("no_text");
-        $(".text1").addClass("add_text");
-        $(".text2").addClass("no_text");
-      }
-
-      else if (y > 1000 && y < 3500){
-        $(".text1").addClass("no_text");
-        $(".text2").removeClass("no_text");
-        $(".text2").addClass("add_text");
-      }
-
-      else{
-        $(".text2").removeClass("add_text");
-        $(".text2").addClass("no_text");
+      if (y > 1000) {
+        $(".text3").removeClass("add_text");
       }
     });
 }
-
-
-
-
-// $(window).resize(function(){
-//
-//   $(document).scroll(function() {
-//     var y = $(this).scrollLeft();
-//
-//     if (y < 659) {
-//       $(".text1").removeClass("no_text");
-//       $(".text1").addClass("add_text");
-//       $(".text2").addClass("no_text");
-//     }
-//
-//     else if (y > 659 && y < 1712){
-//       $(".text1").addClass("no_text");
-//       $(".text2").removeClass("no_text");
-//       $(".text2").addClass("add_text");
-//     }
-//
-//     else{
-//       $(".text2").removeClass("add_text");
-//       $(".text2").addClass("no_text");
-//     }
-//   });
-//
-// });
